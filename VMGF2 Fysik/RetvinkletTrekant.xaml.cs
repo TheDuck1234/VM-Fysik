b0eadf4c401c853562481e8d1d492d5076f0b646
Anders Petersen <madsanders34@msn.com>
2016-02-07 23:26:55 +0100

    new frame

---
+++ VMGF2 Fysik/RetvinkletTrekant.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VMGF2_Fysik
+{
+    /// <summary>
+    /// Interaction logic for RetvinkletTrekant.xaml
+    /// </summary>
+    public partial class RetvinkletTrekant : Window
+    {
+        public RetvinkletTrekant()
+        {
+            InitializeComponent();
+        }
+    }
+}
